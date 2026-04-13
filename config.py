d775362c68bd2e40da1f6a79954153c64f77a74a
dragokepapa <eriyerox683@gmail.com>
2026-04-14 04:05:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
 # ── Support links ──────────────────────────────────────────────────────────────
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OnyxCoders")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", https://t.me/+aRsxufC6pOpiZWFl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+aRsxufC6pOpiZWFl")
 
 # ── Assistant auto-leave ───────────────────────────────────────────────────────
 AUTO_LEAVING_ASSISTANT = False
